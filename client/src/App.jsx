3ad3bff24f28ac259330eef7f9b03112c99c912e
Kasif17 <developer786kasif@gmail.com>
2024-02-03 12:16:03 +0530

    Add router-dom and add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import SignIn from "./Pages/Signin"
+import SingUp from "./Pages/SingUp"
+import Profile from "./Pages/Profile"
 
 export default function App() {
-  return (
-    <h1 className='text-red-500 text-3xl font-bold underline'>App</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/sing-in" element={<SignIn/>} />
+      <Route path="/sing-up" element={<SingUp/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+  </BrowserRouter>
 }
+
